663c1e241776a2823ef5c37dd2bc5229c58519cf
NathanShih04 <nathanshih04@gmail.com>
2024-04-10 20:43:51 -0700

    Fibvecc

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -8,7 +8,7 @@
 
 FibVec::FibVec(){
     vCapacity = 1; // size of storage buffer
-    vec = new[vCapacity];
+    vec = new int[vCapacity];
     vCount = 0; // num items
     vNum = 1;
 }
@@ -30,7 +30,7 @@ void FibVec::insert(int value, size_t index){
 }
 
 int FibVec::lookup(size_t index) const{
-    if(index > vCount || index < 0){
+    if(index > vCount){
         std::out_of_range;
     }
 
@@ -46,7 +46,7 @@ void FibVec::push(int value){
 }
 
 int FibVec::remove(size_t index){
-    if(index > vCount || index < 0){
+    if(index > vCount){
         std::out_of_range;
     }
 
