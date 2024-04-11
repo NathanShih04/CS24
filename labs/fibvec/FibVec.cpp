1335e1acdde9bed4d03c6333423c3dae4f223092
NathanShih04 <nathanshih04@gmail.com>
2024-04-10 19:39:02 -0700

    fibVec

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -5,3 +5,52 @@
 
 
 // FibVec Function Implementations
+
+FibVec::FibVec(){
+    int *vec;
+    size_t vCapacity = 1; // size of storage buffer
+    size_t vCount = 0; // num items
+    size_t vNum;
+}
+
+FibVec::~FibVec(){
+    delete[] vec;
+}
+
+size_t FibVec::capacity() const{
+    return vCapacity;
+}
+
+size_t FibVec::count() const{
+    return vCount;
+}
+
+void FibVec::insert(int value, size_t index){
+
+}
+
+int FibVec::lookup(size_t index) const{
+    if(index > vCount || index < 0){
+        std::out_of_range;
+    }
+
+    return vec[index];
+}
+
+int FibVec::pop(){
+    remove(vCount);
+}
+
+void FibVec::push(int value){
+
+}
+
+int FibVec::remove(size_t index){
+    if(index > vCount || index < 0){
+        std::out_of_range;
+    }
+
+    for(int i = 0; i < vCount - index; i++){
+        vec[i] = vec[i + 1];
+    }
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -6,13 +6,32 @@
 
 class FibVec {
   // Member Variables
-
+  int *vec;
+  size_t vCapacity;
+  size_t vCount;
+  size_t vNum;
   // Helper Functions
 
 public:
   // Constructor and Destructor
+  FibVec();
+
+  ~FibVec();
 
   // Member Functions
+  size_t capacity() const;
+
+  size_t count() const;
+
+  void insert(int value, size_t index);
+
+  int lookup(size_t index) const;
+
+  int pop();
+
+  void push(int value);
+
+  int remove(size_t index);
 
 };
 
