32654edc05aca38e198e64f4df54ba20241137d3
NathanShih04 <nathanshih04@gmail.com>
2024-04-17 12:58:24 -0700

    FIRST PUSSHHH PLS WORKKKKK

--- labs/tictac/Move.cpp
+++ labs/tictac/Move.cpp
@@ -1,4 +1,60 @@
 #include "Errors.h"
 #include "Move.h"
+#include <string>
+#include <sstream>
+#include <bits/stdc++.h>
+using namespace std;
 
 // Space for implementing Move functions.
+
+Move::Move(const std::string& input){
+
+    vector<string> commands;
+    string command;
+    stringstream newInput(input);
+
+    // stores commands seperated by spaces into vector
+    while(newInput >> command){
+        commands.push_back(command);
+
+    }
+
+    // checks to see if format is correct
+    if(commands.size() < 3){
+        throw ParseError("Parse error.");
+    }
+
+    else if(commands[0].length() != 1 || isdigit(commands[0][0]) == false){
+        throw ParseError("Parse error.");
+    }
+
+    else if((tolower(commands[1][0]) != 'x' && tolower(commands[1][0]) != 'x') || commands[1].length() != 1){
+        throw ParseError("Parse error.");
+    }
+
+    else if(tolower(commands[2][0]) != 'a' && tolower(commands[2][0]) != 'b' && tolower(commands[2][0]) != 'c'){
+        throw ParseError("Parse error.");
+    }
+
+    else if(commands[2][1] - '0' != 1 && commands[2][1] - '0' != 2 && commands[2][1] - '0' != 3){
+        throw ParseError("Parse error.");
+    }
+
+    // set vector elements to member variables
+    number = stoi(commands[0]);
+    player = commands[1][0];
+
+    if(tolower(commands[2][0]) == 'a'){
+        commands[2][0] = 1;
+    }
+    else if(tolower(commands[2][0]) == 'b'){
+        commands[2][0] = 2;
+    }
+    else if(tolower(commands[2][0]) == 'c'){
+        commands[2][0] = 3;
+    }
+
+    row = commands[2][0];
+    column = commands[2][1] - '0';
+
+}
