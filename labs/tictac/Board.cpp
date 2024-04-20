d2664e46998b6f272ee66c9f34c7c1c7bbfdc928
NathanShih04 <nathanshih04@gmail.com>
2024-04-20 16:20:33 -0700

    what am i doing

--- labs/tictac/Board.cpp
+++ labs/tictac/Board.cpp
@@ -1,4 +1,10 @@
 #include "Errors.h"
 #include "Board.h"
+#include <string.h>
 
 // Space for implementing Board functions.
+Board::Board(const std::string& input){
+    // Do some getline thingy
+    // what am i doing :(??????
+    if(Move(input) )
+}
--- labs/tictac/Board.h
+++ labs/tictac/Board.h
@@ -6,4 +6,11 @@
 // I recommended writing a Board class to manage your game state.
 // Here's some space for the class definition; member functions go in Board.cpp.
 
+struct Board{
+
+    // Constructor finds and displays game status.
+    Board(const std::string& input);
+};
+
+
 #endif
