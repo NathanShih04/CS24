a356333f53e11cf84a8c538c7912e2da92fcf494
NathanShih04 <nathanshih04@gmail.com>
2024-04-22 00:27:58 -0700

    CMON MAN

--- labs/tictac/game-check.cpp
+++ labs/tictac/game-check.cpp
@@ -10,18 +10,26 @@ int main() {
     string userMove;
     Board board;
 
-    while (getline(cin, userMove)) {
-        try {  
+    while(getline(cin, userMove)){
+        try {
+            Move move(userMove);
             board.check(userMove);
             board.winner();
+            return 0;
         } 
-        catch(const std::exception& e) {
-            // Some error thingies???
+        catch(const ParseError& e) {
+            std::cout << "Parse error: " << e.what() << '\n';
+            return 1;
         }
+        catch (const InvalidMove& e) {
+            std::cout << "Invalid move error: " << e.what() << '\n';
+            return 2; // Return 2 if it's an InvalidMove
+        }
+
     }
 
     board.winner();
-    board.gameEval();
+    board.gameEval();   
 
     return 0;
 }
