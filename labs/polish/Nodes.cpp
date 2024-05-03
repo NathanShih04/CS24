4408c4ebdfee8f7a17fe5bea62ee26321a438500
NathanShih04 <nathanshih04@gmail.com>
2024-05-02 21:44:18 -0700

    nodes stuff

--- labs/polish/Nodes.cpp
+++ labs/polish/Nodes.cpp
@@ -12,3 +12,91 @@ std::string format(double number) {
 
 
 // Implement your AST subclasses' member functions here.
+std::string Number::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Number::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Number::value() const {
+    // Add your code here
+}
+
+// Implement prefix function here
+std::string Addition::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Addition::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Addition::value() const {
+    // Add your code here
+}
+
+// Implement prefix function here
+std::string Subtraction::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Subtraction::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Subtraction::value() const {
+    // Add your code here
+}
+
+// Implement prefix function here
+std::string Multiplication::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Multiplication::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Multiplication::value() const {
+    // Add your code here
+}
+
+// Implement prefix function here
+std::string Division::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Division::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Division::value() const {
+    // Add your code here
+}
+
+// Implement prefix function here
+std::string Modulo::prefix() const {
+    // Add your code here
+}
+
+// Implement postfix function here
+std::string Modulo::postfix() const {
+    // Add your code here
+}
+
+// Implement value function here
+double Modulo::value() const {
+    // Add your code here
+}
--- labs/polish/Nodes.h
+++ labs/polish/Nodes.h
@@ -4,6 +4,75 @@
 #include "AST.h"
 
 // Declare your AST subclasses here.
-// Implement their member functions in Nodes.cpp.
+#include <string>
+using namespace std;
+
+class Number : public AST {
+private:
+    int value;
+public:
+
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+};
+
+// ----------------------------------------------------------------
+
+class Operator : public AST {
+protected:
+    AST* L;
+    AST* R;
+public:
+    Operator(AST* left, AST* right) : L(left), R(right) {}
+    virtual ~Operator();
+};
+
+class Addition : public Operator {
+public:
+    Addition(AST* left, AST* right) : Operator(left, right) {}
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+
+};
+
+class Subtraction : public Operator {
+public:
+    Subtraction(AST* left, AST* right) : Operator(left, right) {}
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+};
+
+class Multiplication : public Operator {
+public:
+    Multiplication(AST* left, AST* right) : Operator(left, right) {}
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+};
+
+class Division : public Operator {
+public:
+    Division(AST* left, AST* right) : Operator(left, right) {}
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+};
+
+class Modulo : public Operator {
+public:
+    Modulo(AST* left, AST* right) : Operator(left, right) {}
+    std::string prefix() const override;
+    std::string postfix() const override;
+    double value() const override;
+};
+
+// ----------------------------------------------------------------
+
+class Negate : public AST {
+
+};
 
 #endif
