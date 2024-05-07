d3a65f3cb84e4626ef7b24e2e237399e5933a149
NathanShih04 <nathanshih04@gmail.com>
2024-05-06 17:38:47 -0700

    klasjdf

--- labs/polish/Stack.cpp
+++ labs/polish/Stack.cpp
@@ -15,32 +15,27 @@ void Stack::push(AST* node) {
 }
 
 AST* Stack::pop() {
-    if (count == 0) {
+    if(count == 0){
         return nullptr;
     }
 
     count--;
-
-    // Store the popped data
-    int size = 500;
     AST* poppedData = stack[count];
 
-    // Create a new stack with reduced size
-    AST** newStack = new AST*[size];
-    for (int i = 0; i < count; i++) {
+    // Pop off the last node
+    int length = 500;
+    AST** newStack = new AST*[length];
+    for(int i = 0; i < count; i++){
         newStack[i] = stack[i];
     }
-
-    // Delete the top node and the original stack
-    delete stack[count];
+    for(int i = 0; i <= count; i++){
+        delete stack[i];
+    }
     delete[] stack;
-
-    // Update the stack pointer
     stack = newStack;
 
     return poppedData;
 }
-
 
 AST* Stack::peek(){
     return stack[count - 1];
