08e4e6d6897590ae89da6c0b17281c231dd541c6
NathanShih04 <nathanshih04@gmail.com>
2024-05-29 12:31:02 -0700

    ldskfj

--- labs/typo/WordList.cpp
+++ labs/typo/WordList.cpp
@@ -7,27 +7,24 @@ using namespace std;
 
 WordList::WordList(std::istream& stream) {
     string word;
-
-    while(std::getline(stream, word)){
+    while (getline(stream, word)) {
         bool isValid = true;
-
-        for(char c : word){
-            if(!islower(c) || !isalpha(c)){
+        for (char c : word) {
+            if (!islower(c) || !isalpha(c)) {
                 isValid = false;
                 break;
             }
         }
-        if(isValid){
+        if (isValid) {
             mWords.push_back(word);
         }
     }
 }
 
-float calculateScore(const std::vector<Point>& points, const std::string& word) {
+float calculateScore(const vector<Point>& points, const string& word) {
     float totalScore = 0.0f;
     size_t length = points.size();
-
-    for(size_t i = 0; i < length; ++i){
+    for (size_t i = 0; i < length; ++i) {
         char c = word[i];
         const Point& wordPoint = QWERTY[c - 'a'];
         const Point& touchPoint = points[i];
@@ -37,47 +34,24 @@ float calculateScore(const std::vector<Point>& points, const std::string& word) 
         float score = 1.0f / (10 * distanceSquared + 1);
         totalScore += score;
     }
-
     return totalScore / length;
 }
 
-Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
+Heap WordList::correct(const vector<Point>& points, size_t maxcount, float cutoff) const {
     Heap heap(maxcount);
     size_t length = points.size();
-
-    for(const std::string& word : mWords){
-        if(word.length() != length){
+    for (const string& word : mWords) {
+        if (word.length() != length) {
             continue;
         }
-
         float score = calculateScore(points, word);
-
-    //    if(score >= cutoff){
-    //         if(heap.count() < maxcount){
-    //             heap.push(word, score);
-    //         } 
-    //     }
-
-    //     else if(heap.count() >= maxcount){
-    //         heap.pushpop(word, score);
-    //         if(heap.top().score < score){
-    //             heap.pushpop(word, score);
-    //         }
-    //     }
-
-        // if(score < cutoff){
-        //     continue;
-        // }
-
-        if(score >= cutoff){
-            if(heap.count() < maxcount){
+        if (score >= cutoff) {
+            if (heap.count() < maxcount) {
                 heap.push(word, score);
-            } 
-            else{
+            } else if (heap.top().score < score) {
                 heap.pushpop(word, score);
             }
         }
     }
-
     return heap;
 }
