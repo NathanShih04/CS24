c8fabcf283563c48849cf8d5094904ca910c655d
NathanShih04 <nathanshih04@gmail.com>
2024-06-09 01:40:00 -0700

    SLdkfj

--- labs/voxhop/VoxMap.h
+++ labs/voxhop/VoxMap.h
@@ -4,26 +4,31 @@
 #include <istream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
+#include <algorithm>
+
 #include "Point.h"
 #include "Route.h"
+#include "Errors.h"
 
-class VoxMap
-{
-    int width;
-    int depth;
-    int height;
-    std::vector<uint8_t> map;
+class VoxMap {
+  // Member Variables
+  int width, depth, height;
+  std::vector<std::vector<std::vector<bool>>> voxels;
 
-    bool isValidPoint(const Point &point) const;
-    bool isNavigable(const Point &point) const;
-    int index(int x, int y, int z) const;
+  // Helper Functions
+  bool isValidPoint(const Point& p) const;
+  bool isValidVoxel(const Point& p) const;
+  std::vector<Point> getNeighbors(const Point& p) const;
+  int heuristic(const Point& a, const Point& b) const;
+  int index(int x, int y, int z) const;
 
 public:
-    VoxMap(std::istream &stream);
-    ~VoxMap() = default;
+  VoxMap(std::istream& stream);
+  ~VoxMap();
 
-    Route route(Point src, Point dst);
+  Route route(Point src, Point dst);
 };
 
 #endif
